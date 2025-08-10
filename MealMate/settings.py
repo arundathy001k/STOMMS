743e21881b3f10b4d588cf6df70498a364a32100
Arundathy <arundathyaru650@gmail.com>
2025-08-10 18:39:12 +0530

    base

--- MealMate/settings.py
+++ MealMate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--*gt*&#-&5h99ig2m)%w!&a#bpi^!z$*m28l)+s!i+&nm48oh$'
+_SECRET_KEY = 'django-insecure--*gt*&#-&5h99ig2m)%w!&a#bpi^!z$*m28l)+s!i+&nm48oh$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
